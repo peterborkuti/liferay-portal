274e8aa2911383094a16cc86515eaf072f763d33
KocsisDaniel <daniel.kocsis@liferay.com>
2013-10-08 14:00:00 +0200

    LPS-35520 Unrelated SF - Remove unused method parameter

--- portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
@@ -573,8 +573,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 					portletDataContext, content,
 					beginPos + contextPath.length(), endPos);
 
-			FileEntry fileEntry = getFileEntry(
-				portletDataContext, dlReferenceParameters);
+			FileEntry fileEntry = getFileEntry(dlReferenceParameters);
 
 			if (fileEntry == null) {
 				endPos = beginPos - 1;
@@ -1608,9 +1607,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 		return map;
 	}
 
-	protected FileEntry getFileEntry(
-		PortletDataContext portletDataContext, Map<String, String[]> map) {
-
+	protected FileEntry getFileEntry(Map<String, String[]> map) {
 		if (map == null) {
 			return null;
 		}
